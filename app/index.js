13b4f02f529da66a793aaf2476785300088d0097
David <admin@mbp-de-admin.home>
2024-01-06 16:14:33 +0100

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const { createServer } = require('http');
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Shopify CLI App!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
